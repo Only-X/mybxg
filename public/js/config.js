0a67c4c680f357462ed40c2e58f70881658f6e15
jiang <jiang@163.com>
2017-09-05 21:37:00 +0800

    使用模板引擎重构登陆功能

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+require.config({
+	baseUrl : '/public/assets',
+	paths : {
+		jquery : 'jquery/jquery.min',
+		cookie : 'jquery-cookie/jquery.cookie',
+		template : 'artTemplate/template-web',
+		common : '../js/common',
+		login : '../js/login',
+		index : '../js/index',
+		teacherlist : '../js/teacher/list'
+	}
+});
